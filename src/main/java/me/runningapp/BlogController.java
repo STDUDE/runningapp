dd24a9332a609ec8f9a86fd727ea472166c234ec
Yellow Crew <yellow@Yellows-iMac.local>
2018-05-22 19:39:30 +0300

    first init commit

--- src/main/java/me/runningapp/BlogController.java
+++ src/main/java/me/runningapp/BlogController.java
@@ -1,4 +1,4 @@
-package me.salisuwy;
+package me.runningapp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/me/runningapp/MainApplicationClass.java
+++ src/main/java/me/runningapp/MainApplicationClass.java
@@ -1,4 +1,4 @@
-package me.salisuwy;
+package me.runningapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
